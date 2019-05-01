e011976765ddf594136295020a5b7685714a3448
James Reidy <jreidy@cornell.edu>
2019-05-01 15:20:16 -0400

    store only the password for WPMS - does not work the other way!

--- web/wp-config.php
+++ web/wp-config.php
@@ -107,18 +107,9 @@ if ( isset( $_ENV['PANTHEON_ENVIRONMENT'] ) ):
 	if (file_exists($secrets)) {
 		if (($file = file_get_contents($secrets)) !== false) {
 			$smtp = json_decode($file,true);
-			if (isset($smtp['SMTP_USER']) && isset($smtp['SMTP_PW'])) {
+			if (isset(isset($smtp['SMTP_PW'])) {
 				// set SMPT_PW,SMTP_USER on each site (dev,test,live) using terminus secrets
 				define('WPMS_ON', true);
-				define('WPMS_MAIL_FROM', 'libsys-mailer@cornell.edu');
-				define('WPMS_MAIL_FROM_NAME', 'Cornell University Library Web Site');
-				define('WPMS_MAILER', 'smtp'); // Possible values 'smtp', 'mail', or 'sendmail'
-				define('WPMS_SET_RETURN_PATH', 'false'); // Sets $phpmailer->Sender if true
-				define('WPMS_SMTP_HOST', 'email-smtp.us-east-1.amazonaws.com'); // The SMTP mail host
-				define('WPMS_SMTP_PORT', 587); // The SMTP server port number
-				define('WPMS_SSL', 'tls'); // Possible values '', 'ssl', 'tls' - note TLS is not STARTTLS
-				define('WPMS_SMTP_AUTH', true); // True turns on SMTP authentication, false turns it off
-				define('WPMS_SMTP_USER', $smtp['SMTP_USER']); // SMTP authentication username, only used if WPMS_SMTP_AUTH is true
 				define('WPMS_SMTP_PASS', $smtp['SMTP_PW']); // SMTP authentication password, only used if WPMS_SMTP_AUTH is true	define( 'WPMS_SMTP_PASS', $_ENV['SMPT_PW'] );
 			}
 			unset($file, $smtp);
