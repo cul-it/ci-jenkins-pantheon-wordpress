e9f162ba7617b2064180ef7af4ae292b90c73546
jenkins <jenkins@sf-lib-inf-006.serverfarm.cornell.edu>
2019-06-27 15:25:32 -0400

    Build assets for ci-280.

---
+++ web/wp-content/themes/culu/single-staff.php
@@ -0,0 +1,109 @@
+<?php
+/**
+ * The template for displaying single staff post
+ * Template Name: Staff
+ * Template Post Type: post, page
+ *
+ * @link https://developer.wordpress.org/themes/basics/template-hierarchy/#single-post
+ *
+ * @package culu
+ */
+
+get_header();
+?>
+
+<main id="main-content" class="page-interior">
+
+	<?php
+
+	while ( have_posts() ) :
+
+		the_post();
+
+		?>
+
+		<section class="staff-profile" aria-label="Staff profile">
+
+			<?php
+
+			$image = get_field('photo');
+
+			if( !empty($image) ) { ?>
+
+				<img class="staff-photo" src="<?php echo $image['url'];?>" alt="<?php echo $image['alt']; ?>">
+
+			<?php } else { ?>
+
+				<img class="staff-photo" src="<?php echo get_template_directory_uri(); ?>/images/staff/no-photo-profile.png">
+
+			<?php } ?>
+
+			<h2><?php echo the_field('first_name');?>
+					<?php echo the_field('last_name');
+
+					if ( !empty(get_field('degree')) ) {
+					 	echo ', ';
+					 	echo the_field('degree');
+					}
+ 					?>
+ 			</h2>
+
+			<h3><?php echo the_field('title'); ?></h3>
+			<p><a href="mailto:<?php echo the_field('email');?>"><?php echo the_field('email');?></a></p>
+			<p class="staff-phone">Phone: <?php the_field('phone');?></p>
+
+			<?php if ( !empty(get_field('consultation')) ) { ?>
+				<script>
+					jQuery.getScript("https://api3.libcal.com/js/myscheduler.min.js", function() {
+					    jQuery("#<?php echo the_field('consultation'); ?>").LibCalMySched({iid: 973, lid: 0, gid: 0, uid: 18275, width: 560, height: 680, title: 'Make an Appointment', domain: 'https://api3.libcal.com'});
+					});
+				</script>
+				<?php } ?>
+
+				<?php
+						if ( !empty(get_field('consultation')) ) {
+				?>
+
+				<p>
+					<a href="#" id="<?php echo the_field('consultation');?>" class="btn-graphic">Book a Consultation</a>
+				</p>
+
+			<?php } ?>
+
+			</div>
+
+		</section>
+
+		<?php
+
+		//the_post_navigation();
+
+		edit_post_link(
+			sprintf(
+				wp_kses(
+					/* translators: %s: Name of current post. Only visible to screen readers */
+					__( 'Edit <span class="screen-reader-text">%s</span>', 'culu' ),
+					array(
+						'span' => array(
+							'class' => array(),
+						),
+					)
+				),
+				get_the_title()
+			),
+			'<span class="edit-link">',
+			'</span>'
+		);
+
+	endwhile; // End of the loop.
+
+	?>
+
+</main><!-- #main -->
+
+<?php
+
+//get_sidebar();
+get_footer();
+
+?>
