1abe9542d36e702a13d1304e5f385f60438aba70
jenkins <jenkins@sf-lib-inf-006.serverfarm.cornell.edu>
2019-08-19 14:33:29 -0400

    Build assets for ci-298.

---
+++ web/wp-content/themes/culu/footer.php
@@ -0,0 +1,121 @@
+<?php
+/**
+ * The template for displaying the footer
+ *
+ * Contains the closing of the #content div and all content after.
+ *
+ * @link https://developer.wordpress.org/themes/basics/template-files/#template-partials
+ *
+ * @package culu
+ */
+
+?>
+
+<footer aria-label="footer navigation">
+
+    <div class="footer__nav">
+
+      <div class="all-libraries">
+        <a href="https://www.library.cornell.edu/"><span class="fa fa-arrow-left" aria-hidden="true"></span> ALL LIBRARIES</a> | <a href="#">Cornell Library Hours</a> | <a href="https://www.library.cornell.edu/ask/email">Ask a Librarian</a>
+      </div>
+
+      <nav aria-label="footer navigation">
+        <?php
+        	wp_nav_menu( array(
+        		'theme_location' => 'footer',
+        		'menu_id'        => 'footer-menu'
+        	) );
+      	?>
+      </nav>
+    </div>
+
+    <!-- Declare variables for theme options-->
+    <!-- Use Kirki to handle customizer -->
+    <?php
+      $college_label = get_theme_mod( 'college_label', '' );
+      $college_link = get_theme_mod( 'college_link', '' );
+      $college_logo = get_theme_mod( 'college_logo', '' );
+      $address_label = get_theme_mod( 'address_label', '' );
+      $city_label = get_theme_mod( 'city_label', '' );
+      $state_label = get_theme_mod( 'state_label', '' );
+      $zip_label = get_theme_mod( 'zip_label', '' );
+      $google_map_label = get_theme_mod( 'google_map_label', '' );
+      $circulation_number_label = get_theme_mod( 'circulation_number_label', '' );
+      $reference_number_label = get_theme_mod( 'reference_number_label', '' );
+      $email_label = get_theme_mod( 'email_label', '' );
+    ?>
+
+    <?php if ( !empty( $college_link ) ){ ?>
+
+    <figure class="college">
+
+      <a href="<?php echo $college_link ?>">
+        <img src="<?php echo get_domain_path( $college_logo );?>" alt="<?php echo $college_label ?> logo"/>
+      </a>
+
+    </figure>
+
+    <?php } ?>
+
+    <address class="library">
+
+        <div class="container">
+
+          <div class="c-1">
+            <p>CONTACT</p>
+            <p><strong><?php echo get_bloginfo( 'name' ); ?></strong><br>
+
+            <?php if ( !empty( $address_label ) ){ ?>
+
+            <?php echo $address_label ?><br>
+            <?php echo $city_label ?>, <?php echo $state_label ?> <?php echo $zip_label ?>
+            <a href="<?php echo $google_map_label ?>"> <i class="fas fa-map-marker-alt" aria-hidden="true" aria-label="Library Location"></i></a></p>
+
+            <?php } ?>
+
+          </div>
+
+          <div class="c-2">
+
+            <?php
+              // Grabbing Kirki Repeater Field and Assigning Variable
+            $settings = get_theme_mod( 'section_contact_email_phones_setting' );
+            ?>
+
+            <?php foreach( $settings as $setting ) : ?>
+
+            <p><?php echo $setting['contact_title'] . ': ';
+
+              $email = $setting['contact_value'];
+
+              if ( checkEmail($email) ) {
+                  echo '<a href="mailto:' . $email . '">' . $email . '</a></p>';
+              } else {
+                  echo $setting['contact_value'] . '</p>';
+              }
+              endforeach;
+              ?>
+              
+          </div>
+
+    </address>
+
+    <address class="cornell">
+
+      <p><?php echo date('Y'); ?> Cornell University Library, Ithaca, NY 14853 | (607) 255-0000 | <a href="https://www.library.cornell.edu/privacy">Privacy</a> | <a href="https://www.library.cornell.edu/web-accessibility">Web Accessibility Assistance</a> | <a href="/admin">Staff Login</a></p>
+
+    </address>
+
+    <p class="feedback">
+
+      <a class="btn-graphic" href="https://www.library.cornell.edu/feedback?destination=web-accessibility">FEEDBACK</a>
+      <a class="btn-graphic" href="https://alumni.library.cornell.edu/content/give-library">GIVE TO THE LIBRARY <span class="fa fa-arrow-right" aria-hidden="true"></span></a>
+
+    </p>
+
+  </footer>
+
+<?php wp_footer(); ?>
+
+</body>
+</html>
