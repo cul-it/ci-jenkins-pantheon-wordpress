3087419b581b089fd2daa6df6b65a90d4440ed2b
jenkins <jenkins@aws-110-164.internal.library.cornell.edu>
2020-06-11 17:42:55 -0400

    Build assets for ci-409.

---
+++ web/wp-content/plugins/acf-better-search/resources/components/notices/acf.php
@@ -0,0 +1,10 @@
+<div class="notice notice-error is-dismissible">
+  <div class="acfbsContent acfbsContent--notice">
+    <h4>
+      <?= __('ACF: Better Search error!', 'acf-better-search'); ?>
+    </h4>
+    <p>
+      <?= __('Unfortunately, but this plugin requires the Advanced Custom Field plugin version 5 for all functionalities to work properly. Please install the latest version of the ACF plugin.', 'acf-better-search'); ?>
+    </p>
+  </div>
+</div>
---
+++ web/wp-content/plugins/filebird/includes/class-filebird-deactivator.php
@@ -0,0 +1,38 @@
+<?php
+
+/**
+ * Fired during plugin deactivation
+ *
+ * @link       https://ninjateam.org
+ * @since      1.0.0
+ *
+ * @package    FileBird
+ * @subpackage FileBird/includes
+ */
+
+/**
+ * Fired during plugin deactivation.
+ *
+ * This class defines all code necessary to run during the plugin's deactivation.
+ *
+ * @since      1.0.0
+ * @package    FileBird
+ * @subpackage FileBird/includes
+ * @author     Ninja Team <support@ninjateam.org>
+ */
+class FileBird_Deactivator
+{
+
+    /**
+     * Short Description. (use period)
+     *
+     * Long Description.
+     *
+     * @since    1.0.0
+     */
+    public static function deactivate()
+    {
+
+    }
+
+}
