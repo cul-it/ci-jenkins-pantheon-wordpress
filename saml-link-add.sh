41e6554d0e99f9aa5b3560ccf29296897f0ab46c
James Reidy <jreidy@cornell.edu>
2023-02-06 12:15:51 -0500

    cul-it/uls#1175 - make directories before trying to add links

--- saml-link-add.sh
+++ saml-link-add.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 # former part of composer.json post-install-cmd
 # run this after composer install
+mkdir -p ./vendor/simplesamlphp/simplesamlphp/config
+mkdir -p ./vendor/simplesamlphp/simplesamlphp/metadata
 ln -s ../../../../private/simplesaml/config/config.php ./vendor/simplesamlphp/simplesamlphp/config/config.php
 ln -s ../../../../private/simplesaml/config/authsources.php ./vendor/simplesamlphp/simplesamlphp/config/authsources.php
 ln -s ../../../../private/simplesaml/metadata/saml20-idp-remote.php ./vendor/simplesamlphp/simplesamlphp/metadata/saml20-idp-remote.php
