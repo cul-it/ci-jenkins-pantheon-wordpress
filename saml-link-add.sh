0a95af46329d3f55c0943f9bc836a4e7c76ee285
James Reidy <jreidy@cornell.edu>
2023-02-06 12:16:32 -0500

    cul-it/uls#1175 - overwrite if links already exist with -f

--- saml-link-add.sh
+++ saml-link-add.sh
@@ -3,7 +3,7 @@
 # run this after composer install
 mkdir -p ./vendor/simplesamlphp/simplesamlphp/config
 mkdir -p ./vendor/simplesamlphp/simplesamlphp/metadata
-ln -s ../../../../private/simplesaml/config/config.php ./vendor/simplesamlphp/simplesamlphp/config/config.php
-ln -s ../../../../private/simplesaml/config/authsources.php ./vendor/simplesamlphp/simplesamlphp/config/authsources.php
-ln -s ../../../../private/simplesaml/metadata/saml20-idp-remote.php ./vendor/simplesamlphp/simplesamlphp/metadata/saml20-idp-remote.php
-ln -s ../../../../private/simplesaml/metadata/shib13-idp-remote.php ./vendor/simplesamlphp/simplesamlphp/metadata/shib13-idp-remote.php
+ln -s -f ../../../../private/simplesaml/config/config.php ./vendor/simplesamlphp/simplesamlphp/config/config.php
+ln -s -f ../../../../private/simplesaml/config/authsources.php ./vendor/simplesamlphp/simplesamlphp/config/authsources.php
+ln -s -f ../../../../private/simplesaml/metadata/saml20-idp-remote.php ./vendor/simplesamlphp/simplesamlphp/metadata/saml20-idp-remote.php
+ln -s -f ../../../../private/simplesaml/metadata/shib13-idp-remote.php ./vendor/simplesamlphp/simplesamlphp/metadata/shib13-idp-remote.php
