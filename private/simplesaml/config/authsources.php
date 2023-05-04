27c190be5be19b81a1843f12ab349e3044043649
James Reidy <jreidy@cornell.edu>
2023-05-04 11:17:04 -0400

    LP-274 - quote SP_ENTITY_ID in define()

--- private/simplesaml/config/authsources.php
+++ private/simplesaml/config/authsources.php
@@ -13,7 +13,7 @@ if (isset($_SERVER['HTTPS']) && 'on' === $_SERVER['HTTPS']) {
 }
 // Append the host(domain name, ip) to the URL.
 $url .= $_SERVER['HTTP_HOST'];
-define(SP_ENTITY_ID, "$url/simplesaml/module.php/saml/sp/metadata/default-sp");
+define('SP_ENTITY_ID', "$url/simplesaml/module.php/saml/sp/metadata/default-sp");
 
 if (defined('PANTHEON_ENVIRONMENT') && !empty($_ENV['PANTHEON_ENVIRONMENT'])) {
     if ('live' == $_ENV['PANTHEON_ENVIRONMENT']) {
